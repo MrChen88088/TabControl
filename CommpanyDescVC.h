8bbb0920317b790f04439b1e63699d81c015dc77
LEIJING.CHEN <chenljit@163.com>
2017-04-17 13:52:29 +0800

    first commit

---
+++ CommpanyDescVC.h
@@ -0,0 +1,13 @@
+//
+//  CommpanyDescVC.h
+//  FortunePig
+//
+//  Created by MrChen on 17/4/13.
+//  Copyright © 2017年 CHENLEIJING. All rights reserved.
+//
+
+#import "RootBackViewController.h"
+#import "MyLayout.h"
+@interface CommpanyDescVC : RootBackViewController
+
+@end
